a2bdab6b11b54c0bc26b2166ff14761cabf1621c
Matias Valmaggia <mvalmaggia@fi.uba.ar>
2026-03-24 20:03:02 -0300

    fix for server receive

--- server/protocol/protocol.py
+++ server/protocol/protocol.py
@@ -10,12 +10,21 @@ def send_message(socket, message):
     header = length.to_bytes(HEADER_LENGTH, byteorder='big')
     socket.sendall(header + msg_bytes)
 
+def recv_all(sock, n):
+    data = bytearray()
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet: 
+            return None 
+        data.extend(packet)
+    return data
+
 def receive_message(socket):
-    header = socket.recv_all(HEADER_LENGTH)
+    header = recv_all(socket, HEADER_LENGTH)
     if not header:
         return None
 
     length = int.from_bytes(header, byteorder='big')
-    msg_bytes = socket.recv_all(length)
+    msg_bytes = recv_all(socket, length)
     return msg_bytes.decode('utf-8')
 
