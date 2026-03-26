a83ab7a61a8f909a03a0c5aec61ff582d42c38d1
Matias Valmaggia <mvalmaggia@fi.uba.ar>
2026-03-26 00:07:09 -0300

    remove debug prints

--- server/codec/codec.py
+++ server/codec/codec.py
@@ -35,6 +35,4 @@ def encode_winners(winners: list[str]) -> str:
     """
     Encode a list of winners' documents into a string message.
     """
-    print(f"Encoding winners: {winners}")
-    print(f"Encoded winners string: {'|'.join(winners)}")
     return "|".join(winners)
